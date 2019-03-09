5d5e436a45b9b3fb2edff94ca00bf6033ac4f896
Yuri <yuri.tayama@linksports.co.jp>
2019-03-10 06:51:07 +0900

    音声入力の実装

--- src/screen/search/index.js
+++ src/screen/search/index.js
@@ -2,25 +2,121 @@ import React, { Component } from 'react';
 import {
   View,
   Text,
+  Image,
+  TouchableOpacity,
 } from 'react-native';
+import Voice from 'react-native-voice';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
+import images from '../../components/images';
 import styles from './styles';
 
 class SearchScreen extends Component {
   constructor(props) {
     super(props);
+    Voice.onSpeechPartialResults = this.onSpeechPartialResults;
+
     this.state = {
-      //
+      recording: false,
+      partialResults: [],
     };
   }
 
+  componentWillUnmount() {
+    Voice.destroy().then(Voice.removeAllListeners);
+  }
+
   render() {
+    const {
+      recording,
+      partialResults,
+    } = this.state;
+    const hasResults = partialResults.length > 0;
+
     return (
       <View style={styles.container}>
-        <Text>SEARCH SCREEN</Text>
+
+        <Text style={styles.guideTxt}>お酒の名前を教えてください。</Text>
+
+        <View style={styles.voiceContainer}>
+          <View style={styles.cantering}>
+            {this.state.partialResults.map(result => (
+              <Text key={result} ellipsizeMode="wordWrapping" style={styles.stat}>
+                {result}
+              </Text>
+            ))}
+          </View>
+        </View>
+
+        <View style={styles.mikeContainer}>
+          {!recording
+          && (
+          <TouchableOpacity onPress={() => this.onPressStartRecord()}>
+            <Image style={styles.mikeButton} source={images.mike} />
+          </TouchableOpacity>
+          )
+          }
+          {recording
+          && (
+          <TouchableOpacity onPress={() => this.onPressStopRecord()}>
+            <Image style={styles.mikeButton} source={images.pause} />
+          </TouchableOpacity>
+          )
+          }
+        </View>
+
+        <View style={styles.buttonsContainer}>
+          {hasResults && !recording
+          && (
+          <View>
+
+            <TouchableOpacity onPress={() => this.onPressSearch()} style={styles.primaryBtn}>
+              <Text style={styles.primaryBtnTxt}>検索する</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => this.onPressClear()} style={styles.seondaryBtn}>
+              <Text style={styles.secondaryBtnTxt}>クリア</Text>
+            </TouchableOpacity>
+
+          </View>
+          )
+          }
+        </View>
+
       </View>
     );
   }
+
+  onPressStartRecord() {
+    this.setState({ recording: true });
+    Voice.start('ja-JP');
+  }
+
+  onPressStopRecord() {
+    this.setState({ recording: false });
+    Voice.stop();
+  }
+
+  onPressSearch() {
+    // 検索処理
+  }
+
+  onPressClear() {
+    this.setState({ partialResults: [] });
+  }
+
+  onSpeechPartialResults = (e) => {
+    this.setState({
+      partialResults: e.value,
+    });
+  };
 }
 
-export default SearchScreen;
+const mapStatetoProps = (state, props) => ({ state, props });
+
+const mapDispatchToProps = dispatch => (
+  bindActionCreators({}, dispatch)
+);
+
+export default connect(mapStatetoProps, mapDispatchToProps)(SearchScreen);
--- src/screen/search/styles.js
+++ src/screen/search/styles.js
@@ -1,10 +1,86 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
+
+const { width } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#FFF',
+  },
+  guideTxt: {
+    fontSize: 16,
+    marginBottom: 32,
+  },
+  voiceContainer: {
+    flexDirection: 'row',
+    padding: 16,
+    borderColor: '#DDD',
+    borderWidth: 1,
+    width: width - 64,
+    height: 150,
+    justifyContent: 'center',
+    alignContent: 'center',
+    flexWrap: 'wrap',
+  },
+  cantering: {
+    // flexDirection: 'row',
+    // height: 100,
+    justifyContent: 'center',
+    // alignContent: 'center',
+    // alignItems: 'center',
+  },
+  mikeContainer: {
+    marginTop: 32,
+    marginBottom: 32,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  mikeButton: {
+    width: 50,
+    height: 50,
+  },
+  buttonsContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 88,
+  },
+  primaryBtn: {
+    marginBottom: 16,
+    width: 280,
+    height: 36,
+    borderRadius: 18,
+    backgroundColor: 'black',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  seondaryBtn: {
+    width: 280,
+    height: 36,
+    borderRadius: 18,
+    borderWidth: 1,
+    borderColor: 'black',
+    backgroundColor: 'transparent',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  primaryBtnTxt: {
+    fontSize: 16,
+    color: 'white',
+    fontWeight: '600',
+  },
+  seondaryBtnTxt: {
+    fontSize: 16,
+    color: 'black',
+    fontWeight: '600',
+  },
+  stat: {
+    color: '#212121',
+    fontSize: 14,
+    lineHeight: 14 * 1.3,
+    paddingRight: 8,
+    textAlign: 'center',
   },
 });
 
