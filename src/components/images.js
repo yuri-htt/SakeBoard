f9e945c637b9ce8e14082415f624bad044848506
Yuri <yuri.tayama@linksports.co.jp>
2019-03-10 04:30:45 +0900

    画像の追加

---
+++ src/components/images.js
@@ -0,0 +1,33 @@
+const cooktail = require('../../assets/cooktail.png');
+const wine = require('../../assets/wine.png');
+const beer = require('../../assets/beer.png');
+const sake = require('../../assets/sake.png');
+const syotyu = require('../../assets/syotyu.png');
+const whisky = require('../../assets/whisky.png');
+const cooktailWhite = require('../../assets/cooktailWhite.png');
+const wineWhite = require('../../assets/wineWhite.png');
+const beerWhite = require('../../assets/beerWhite.png');
+const sakeWhite = require('../../assets/sakeWhite.png');
+const syotyuWhite = require('../../assets/syotyuWhite.png');
+const whiskyWhite = require('../../assets/whiskyWhite.png');
+const mike = require('../../assets/mike.png');
+const pause = require('../../assets/pause.png');
+
+const images = {
+  cooktail,
+  wine,
+  beer,
+  sake,
+  syotyu,
+  whisky,
+  cooktailWhite,
+  wineWhite,
+  beerWhite,
+  sakeWhite,
+  syotyuWhite,
+  whiskyWhite,
+  mike,
+  pause,
+};
+
+export default images;
