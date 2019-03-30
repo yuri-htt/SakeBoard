4e311f892a34383fb713312bde3fd7109f0c4495
Yuri <yuri.tayama@linksports.co.jp>
2019-03-30 14:30:37 +0900

    検索結果を表示するモーダルのコンポーネントを作成

---
+++ src/components/listModal/index.js
@@ -0,0 +1,125 @@
+import React, { Component } from 'react';
+import {
+  Text,
+  View,
+  Modal,
+  Platform,
+  ScrollView,
+  Dimensions,
+  TouchableOpacity,
+  ActivityIndicator,
+} from 'react-native';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import styles from './styles';
+import * as SearchActions from '../../redux/modules/search';
+import CategoryIcon from '../categoryIcon';
+
+const { width } = Dimensions.get('window');
+
+class ListModal extends Component {
+  render() {
+    const {
+      actions,
+    } = this.props;
+    const {
+      search,
+    } = this.props.state;
+
+    return (
+      <Modal
+        animationType="slide"
+        onRequestClose={() => actions.setModal(false)}
+        transparent
+        visible
+      >
+        <View style={styles.modalContainer}>
+
+          <View style={[styles.modal]}>
+
+            {/* 検索中 */}
+            {search.loading
+            && (
+              <View style={[styles.modal, styles.center]}>
+                <ActivityIndicator size="large" color="#FF9800" />
+              </View>
+            )
+            }
+
+            {/* 検索結果をリスト表示 */}
+            {search.loaded && search.data.length > 0
+            && (
+            <ScrollView
+              showsVerticalScrollIndicator={false}
+              style={{ width }}
+            >
+              {search.data.map(result => (
+                <View key={result.name}>
+                  {this.renderCandidateListCard(result)}
+                </View>
+              ))}
+            </ScrollView>
+            )
+            }
+
+            {/* 検索結果が0件 */}
+            {search.loaded && search.data.length === 0
+            && (
+              <View style={[styles.center, styles.flex]}>
+                <Text>該当0件</Text>
+              </View>
+            )
+            }
+
+          </View>
+
+        </View>
+      </Modal>
+    );
+  }
+
+  renderCandidateListCard(item) {
+    return (
+      <TouchableOpacity onPress={() => this.onPressCard()} key={item.name} style={styles.cardContainer}>
+        <View style={[Platform.OS === 'ios' ? styles.candidateCardforiOS : styles.candidateCardforAndroid]}>
+
+          <CategoryIcon categoryName={item.categoryName} style={styles.icon} />
+
+          <View style={styles.flex}>
+            <Text style={styles.categoryCardTxt} numberOfLines={1} ellipsizeMode="tail">{item.name}</Text>
+            <View style={styles.detail}>
+              {!!item.areaName && !item.companyName
+                && <Text style={styles.detailTxt} numberOfLines={1}>{item.areaName}</Text>
+              }
+              {!!item.companyName && !item.areaName
+                && <Text style={styles.detailTxt} numberOfLines={1}>{item.companyName}</Text>
+              }
+              {!!item.areaName && !!item.companyName
+                && <Text style={styles.detailTxt} numberOfLines={1}>{`${item.areaName}  ${item.companyName}`}</Text>
+              }
+            </View>
+          </View>
+
+        </View>
+      </TouchableOpacity>
+    );
+  }
+
+  onPressCard() {
+    // 選択したお酒を登録する画面へ遷移する
+  }
+}
+
+const mapStatetoProps = (state) => {
+  const { records } = state;
+  return { records };
+};
+
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators({
+    ...SearchActions,
+  }, dispatch),
+});
+
+export default connect(mapStatetoProps, mapDispatchToProps)(ListModal);
---
+++ src/components/listModal/styles.js
@@ -0,0 +1,58 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  flex: {
+    flex: 1,
+  },
+  modalContainer: {
+    alignItems: 'center',
+    backgroundColor: 'white',
+    marginVertical: 32,
+    paddingBottom: 32,
+  },
+  modal: {
+    backgroundColor: 'white',
+    alignItems: 'center',
+    marginTop: 32,
+  },
+  center: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cardContainer: {
+    marginHorizontal: 32,
+  },
+  candidateCardforAndroid: {
+    flexDirection: 'row',
+    marginBottom: 8,
+    borderRadius: 4,
+    padding: 16,
+    alignItems: 'center',
+    elevation: 1,
+  },
+  candidateCardforiOS: {
+    flexDirection: 'row',
+    marginBottom: 8,
+    borderRadius: 4,
+    padding: 16,
+    alignItems: 'center',
+    shadowColor: 'black',
+    shadowOpacity: 0.2,
+    shadowOffset: { width: 2, height: 2 },
+    shadowRadius: 2,
+    borderColor: '#BDBDBD',
+    borderWidth: 1,
+    backgroundColor: '#FFF',
+  },
+  icon: {
+    marginRight: 16,
+  },
+  detail: {
+    flexDirection: 'row',
+  },
+  detailTxt: {
+    color: '#A2A2A2',
+  },
+});
+
+export default styles;
