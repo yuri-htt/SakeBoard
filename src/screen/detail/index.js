24e17637d083f3be6f97af674f41b28cd4c76a35
Yuri <yuri.tayama@linksports.co.jp>
2019-04-01 01:12:20 +0900

    登録情報詳細表示画面

--- src/screen/detail/index.js
+++ src/screen/detail/index.js
@@ -1,26 +1,66 @@
 import React, { Component } from 'react';
 import {
-  View,
   Text,
+  View,
 } from 'react-native';
+import moment from 'moment';
+import StarRating from 'react-native-star-rating';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
+import CategoryIcon from '../../components/categoryIcon';
 import styles from './styles';
 
+// eslint-disable-next-line react/prefer-stateless-function
 class DetailScreen extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      //
-    };
-  }
-
   render() {
+    const { post } = this.props.state;
+    const postedDate = moment(post.timestamp).format('MM月DD日');
+    const selectedPost = post.data;
+
     return (
       <View style={styles.container}>
-        <Text>DETAIL SCREEN</Text>
+        <View style={styles.header}>
+          <CategoryIcon categoryName={selectedPost.categoryName} size={60} style={styles.icon} />
+          <View style={styles.flex}>
+            <Text style={styles.name} numberOfLines={2}>{selectedPost.name}</Text>
+            <View style={styles.detail}>
+              {!!selectedPost.areaName && !selectedPost.companyName
+                && <Text style={styles.detailTxt} numberOfLines={1}>{selectedPost.areaName}</Text>
+              }
+              {!!selectedPost.companyName && !selectedPost.areaName
+                && <Text style={styles.detailTxt} numberOfLines={1}>{selectedPost.companyName}</Text>
+              }
+              {!!selectedPost.areaName && !!selectedPost.companyName
+                && <Text style={styles.detailTxt} numberOfLines={1}>{`${selectedPost.areaName}  ${selectedPost.companyName}`}</Text>
+              }
+            </View>
+          </View>
+        </View>
+        <View style={styles.stars}>
+          <StarRating
+            disabled
+            maxStars={5}
+            rating={selectedPost.starCount}
+            starSize={16}
+            starStyle={{ marginRight: 4 }}
+            containerStyle={{ justifyContent: 'flex-start' }}
+            fullStarColor="orange"
+            emptyStarColor="orange"
+          />
+        </View>
+        <Text style={styles.contentTxt}>{selectedPost.text}</Text>
+        <Text style={styles.dateTxt}>{postedDate}</Text>
+
       </View>
     );
   }
 }
 
-export default DetailScreen;
+const mapStatetoProps = (state, props) => ({ state, props });
+
+const mapDispatchToProps = dispatch => (
+  bindActionCreators({}, dispatch)
+);
+
+export default connect(mapStatetoProps, mapDispatchToProps)(DetailScreen);
--- src/screen/detail/styles.js
+++ src/screen/detail/styles.js
@@ -1,10 +1,38 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
+  flex: {
+    flex: 1,
+  },
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#FFF',
+    paddingHorizontal: 32,
+  },
+  header: {
+    marginVertical: 32,
+    flexDirection: 'row',
     alignItems: 'center',
+  },
+  icon: {
+    marginRight: 16,
+  },
+  detail: {
+    flexDirection: 'row',
+  },
+  detailTxt: {
+    color: '#A2A2A2',
+  },
+  name: {
+    fontSize: 16,
+  },
+  contentTxt: {
+    marginTop: 16,
+    color: '#212121',
+  },
+  dateTxt: {
+    marginTop: 16,
+    color: '#757575',
   },
 });
 
